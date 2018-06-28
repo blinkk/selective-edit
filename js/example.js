458e67f4867c1d83fdef926e228f4e73815f42ed
Randy Merrill <randy@blinkk.com>
2018-06-28 15:13:43 -0600

    Adding a placeholder for seeing the serialized value from the editor.

--- js/example.js
+++ js/example.js
@@ -1,9 +1,36 @@
 import Selective from './selective'
+import {
+  MDCTextField
+} from '@material/textfield'
 
 const configEl = document.querySelector('#config')
+const configMdc = document.querySelector('.content__config .mdc-text-field')
 const dataEl = document.querySelector('#data')
+const dataMdc = document.querySelector('.content__data .mdc-text-field')
 const fieldsEl = document.querySelector('#fields')
+const valueEl = document.querySelector('#value')
+const valueMdc = document.querySelector('.content__value .mdc-text-field')
+
+// -----------------------------------------------------------
+// Basic example of using Selective.
+// -----------------------------------------------------------
 
 const exampleSelective = new Selective(fieldsEl, JSON.parse(configEl.value))
 
 exampleSelective.data = JSON.parse(dataEl.value)
+
+
+// -----------------------------------------------------------
+// Functionality for making the example page function.
+// -----------------------------------------------------------
+
+// Create the example MDC components.
+new MDCTextField(configMdc)
+new MDCTextField(dataMdc)
+new MDCTextField(valueMdc)
+
+const handleValueChange = (e) => {
+  console.log(valueEl);
+  valueEl.textContent = JSON.stringify(exampleSelective.value, null, 2)
+}
+handleValueChange()
