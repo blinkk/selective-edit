7d03763cd8e8446a359353bf718f1b1ae119c79e
Randy Merrill <randy@blinkk.com>
2019-12-19 14:00:00 -0700

    Bringing the project back up to date and initial running state.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
-var extend = require('deep-extend');
-var fs = require('fs');
-var gulp = require('gulp');
-var gulpAutoprefixer = require('gulp-autoprefixer');
-var path = require('path');
-var readdirRecursive = require('fs-readdir-recursive');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var server = require('gulp-webserver');
-var webpack = require('webpack');
-var webpackStream = require('webpack-stream');
-var WebpackBabiliPlugin = require("babili-webpack-plugin");
+const connect = require('gulp-connect');
+const extend = require('deep-extend');
+const fs = require('fs');
+const {
+  dest,
+  series,
+  parallel,
+  src,
+  task,
+  watch
+} = require('gulp');
+const gulpAutoprefixer = require('gulp-autoprefixer');
+const path = require('path');
+const readdirRecursive = require('fs-readdir-recursive');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 
-var config = {
+const config = {
   JS_SOURCE_DIR: './js/',
   JS_SOURCES: [
     './js/**/*.js',
@@ -22,23 +28,24 @@ var config = {
       mangle: false
     }
   },
-  SASS_SOURCE_DIR: './sass/',
+  SASS_SOURCE_DIR: './sass/**/*.{sass,scss}',
   SASS_SOURCES: [
-    './sass/**/*.sass',
+    './sass/**/*.{sass,scss}',
+    './node_modules/@material/**/*.{sass,scss}',
   ],
   SASS_OUT_DIR: './dist/css/'
 };
 
-var jsFiles = fs.readdirSync(config.JS_SOURCE_DIR);
-var entry = {};
-jsFiles.forEach(function (value) {
+const jsFiles = readdirRecursive(config.JS_SOURCE_DIR);
+const entry = {};
+jsFiles.forEach(function(value) {
   if (value.endsWith('.js')) {
-    var key = value.substring(0, value.length - 3);
+    const key = value.substring(0, value.length - 3);
     entry[key] = config.JS_SOURCE_DIR + value;
   }
 });
 
-var webpackConfig = {
+const webpackConfig = {
   entry: entry,
   mode: 'development',
   output: {
@@ -46,57 +53,57 @@ var webpackConfig = {
     filename: '[name].min.js'
   }
 };
-var webpackProdConfig = extend({
+
+const webpackProdConfig = extend({}, webpackConfig, {
   mode: 'production',
-}, webpackConfig);
-
-gulp.task('compile-js', function() {
-  return gulp.src(config.JS_SOURCES)
-      .pipe(webpackStream(
-        webpackProdConfig, webpack
-      ))
-      .pipe(gulp.dest(config.JS_OUT_DIR));
 });
 
-gulp.task('watch-js', () => {
-  webpackConfig.watch = true;
+const webpackWatchConfig = extend({}, webpackConfig, {
+  watch: true,
+});
 
-  gulp.src(config.JS_SOURCES)
+task('compile-js', function() {
+  return src(config.JS_SOURCES)
     .pipe(webpackStream(
-      webpackConfig, webpack
+      webpackProdConfig, webpack
     ))
-    .pipe(gulp.dest(config.JS_OUT_DIR));
+    .pipe(dest(config.JS_OUT_DIR));
 });
 
-gulp.task('compile-sass', function() {
-  gulp.src(`${config.SASS_SOURCE_DIR}**/*.sass`)
-  .pipe(sass({
-    includePaths: ['./node_modules/'],
-    outputStyle: 'compressed'
-  })).on('error', sass.logError)
-  .pipe(rename(function(path) {
-    path.basename += '.min';
-  }))
-  .pipe(gulpAutoprefixer({
-    browsers: [
-      'last 1 version',
-      'last 2 iOS versions'
-    ],
-  }))
-  .pipe(gulp.dest(config.SASS_OUT_DIR));
+task('compile-sass', function(cb) {
+  return src(config.SASS_SOURCE_DIR)
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: [
+        'node_modules'
+      ],
+    })).on('error', sass.logError)
+    .pipe(rename(function(path) {
+      path.basename += '.min';
+    }))
+    .pipe(gulpAutoprefixer())
+    .pipe(dest(config.SASS_OUT_DIR));
 });
 
-gulp.task('watch-sass', function() {
-  gulp.watch(config.SASS_SOURCES, ['compile-sass']);
+task('watch-sass', function() {
+  watch(config.SASS_SOURCES, series('compile-sass'));
 });
 
-gulp.task('server', function() {
-  gulp.src(['example/', 'dist/'])
-    .pipe(server({
-      livereload: true,
-      port: 8888,
-    }));
+task('watch-js', function() {
+  src(config.JS_SOURCES)
+  .pipe(webpackStream(webpackWatchConfig, webpack))
+  .pipe(dest(config.JS_OUT_DIR));
 });
 
-gulp.task('build', ['compile-js', 'compile-sass']);
-gulp.task('default', ['compile-sass', 'watch-js', 'watch-sass', 'server']);
+task('grow-build', parallel('compile-js', 'compile-sass'))
+
+task('connect', function() {
+  connect.server({
+    root: ['example', 'dist'],
+    livereload: true,
+    port: 8888,
+  });
+});
+exports.build = parallel('compile-js', 'compile-sass')
+exports.server = series(exports.build, parallel('connect', 'watch-js', 'watch-sass'))
+exports.default = exports.server
