b46abeb9d7c43811dc8044600064d98ea9608eed
Zoramite <Zoramite@users.noreply.github.com>
2021-01-14 19:26:18 +0000

    deploy: 80d3b2ad7837b160318098294c6c433d215b23bc

--- example/example.js
+++ example/example.js
@@ -1,11 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
+const deepObject_1 = require("./utility/deepObject");
 const configEl = document.querySelector('#config');
+const dataEl = document.querySelector('#data');
 const fieldsEl = document.querySelector('#fields');
 /**
  * Basic example of using -selective editor.
  */
 const editorConfig = JSON.parse(configEl.value || '');
 const exampleSelective = new index_1.SelectiveEditor(editorConfig, fieldsEl);
+exampleSelective.data = deepObject_1.autoDeepObject(JSON.parse(dataEl.value));
+exampleSelective.render();
+// Bind to the custom event to re-render the editor.
+document.addEventListener('selective.render', () => {
+    exampleSelective.render();
+});
 //# sourceMappingURL=example.js.map
