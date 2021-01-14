5741efdb0350fa3ac6e4470c21ff4b366fd1d400
Zoramite <Zoramite@users.noreply.github.com>
2021-01-14 17:40:16 +0000

    deploy: 02c1bebdcc73019994f7861070b961b7863dcacb

---
+++ example/example.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("./index");
+const configEl = document.querySelector('#config');
+const fieldsEl = document.querySelector('#fields');
+/**
+ * Basic example of using -selective editor.
+ */
+const editorConfig = JSON.parse(configEl.value || '');
+const exampleSelective = new index_1.SelectiveEditor(fieldsEl, editorConfig);
+//# sourceMappingURL=example.js.map
