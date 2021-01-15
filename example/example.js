5e3cd64c81bc01bc838cd859f5bbfb38e19b17fe
Zoramite <Zoramite@users.noreply.github.com>
2021-01-15 20:11:38 +0000

    deploy: b8e001a9489f358518aeb2097dcd76a9260cf0bf

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
 const field_1 = require("./selective/field");
+const index_1 = require("./index");
 const deepObject_1 = require("./utility/deepObject");
 const configEl = document.querySelector('#config');
 const dataEl = document.querySelector('#data');
@@ -12,7 +12,10 @@ const fieldsEl = document.querySelector('#fields');
 const editorConfig = JSON.parse(configEl.value || '');
 const exampleSelective = new index_1.SelectiveEditor(editorConfig, fieldsEl);
 // Add the field types.
-exampleSelective.addFieldType('text', field_1.TextField);
+exampleSelective.addFieldTypes({
+    text: field_1.TextField,
+    textarea: field_1.TextareaField,
+});
 exampleSelective.data = deepObject_1.autoDeepObject(JSON.parse(dataEl.value));
 exampleSelective.render();
 // Bind to the custom event to re-render the editor.
