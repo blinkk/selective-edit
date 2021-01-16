de25bde73e9fb83adb630517b2be8180f96a4eef
Zoramite <Zoramite@users.noreply.github.com>
2021-01-16 00:07:44 +0000

    deploy: c2bc7ccbd09f3272561746f0d8cda51638f75b51

--- example/example.js
+++ example/example.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const field_1 = require("./selective/field");
+const events_1 = require("./selective/events");
+const list_1 = require("./selective/field/list");
 const index_1 = require("./index");
+const text_1 = require("./selective/field/text");
+const textarea_1 = require("./selective/field/textarea");
 const deepObject_1 = require("./utility/deepObject");
 const configEl = document.querySelector('#config');
 const dataEl = document.querySelector('#data');
 const fieldsEl = document.querySelector('#fields');
+const valueEl = document.querySelector('#value');
 /**
  * Basic example of using -selective editor.
  */
@@ -13,13 +17,18 @@ const editorConfig = JSON.parse(configEl.value || '');
 const exampleSelective = new index_1.SelectiveEditor(editorConfig, fieldsEl);
 // Add the field types.
 exampleSelective.addFieldTypes({
-    text: field_1.TextField,
-    textarea: field_1.TextareaField,
+    list: list_1.ListField,
+    text: text_1.TextField,
+    textarea: textarea_1.TextareaField,
 });
 exampleSelective.data = deepObject_1.autoDeepObject(JSON.parse(dataEl.value));
-exampleSelective.render();
 // Bind to the custom event to re-render the editor.
-document.addEventListener('selective.render', () => {
+document.addEventListener(events_1.EVENT_RENDER, () => {
     exampleSelective.render();
 });
+// Show value after every render as an example.
+document.addEventListener(events_1.EVENT_RENDER_COMPLETE, () => {
+    valueEl.textContent = JSON.stringify(exampleSelective.value, null, 2);
+});
+exampleSelective.render();
 //# sourceMappingURL=example.js.map
