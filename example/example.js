3c1aa84a2f82c9f942ac1b06a624196e206f4691
Zoramite <Zoramite@users.noreply.github.com>
2021-01-19 15:10:00 +0000

    deploy: 87e0de776d22416f69ecedb9b3a32c51e156eaf9

--- example/example.js
+++ example/example.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const events_1 = require("./selective/events");
 const group_1 = require("./selective/field/group");
+const length_1 = require("./selective/rule/length");
 const list_1 = require("./selective/field/list");
+const match_1 = require("./selective/rule/match");
+const pattern_1 = require("./selective/rule/pattern");
+const range_1 = require("./selective/rule/range");
+const require_1 = require("./selective/rule/require");
 const index_1 = require("./index");
 const text_1 = require("./selective/field/text");
 const textarea_1 = require("./selective/field/textarea");
@@ -12,6 +17,8 @@ const configEl = document.querySelector('#config');
 const dataEl = document.querySelector('#data');
 const fieldsEl = document.querySelector('#fields');
 const guessEl = document.querySelector('.content__data__actions button');
+const statusCleanEl = document.querySelector('.status__clean');
+const statusValidEl = document.querySelector('.status__valid');
 const valueEl = document.querySelector('#value');
 /**
  * Basic example of using -selective editor.
@@ -26,6 +33,14 @@ exampleSelective.addFieldTypes({
     textarea: textarea_1.TextareaField,
     variant: variant_1.VariantField,
 });
+// Add the field types.
+exampleSelective.addRuleTypes({
+    length: length_1.LengthRule,
+    match: match_1.MatchRule,
+    pattern: pattern_1.PatternRule,
+    range: range_1.RangeRule,
+    require: require_1.RequireRule,
+});
 exampleSelective.data = deepObject_1.autoDeepObject(JSON.parse(dataEl.value));
 // Bind to the custom event to re-render the editor.
 document.addEventListener(events_1.EVENT_RENDER, () => {
@@ -34,6 +49,9 @@ document.addEventListener(events_1.EVENT_RENDER, () => {
 // Show value after every render as an example.
 document.addEventListener(events_1.EVENT_RENDER_COMPLETE, () => {
     valueEl.textContent = JSON.stringify(exampleSelective.value, null, 2);
+    // Update status.
+    updateStatus(statusCleanEl, exampleSelective.isClean);
+    updateStatus(statusValidEl, exampleSelective.isValid);
 });
 // Allow guessing config based on data.
 guessEl.addEventListener('click', () => {
@@ -54,4 +72,20 @@ guessEl.addEventListener('click', () => {
     configEl.textContent = JSON.stringify({ fields: prettyFields }, null, 2);
 });
 exampleSelective.render();
+function updateStatus(element, isTrue) {
+    if (isTrue) {
+        element.textContent = 'True';
+        element.classList.add('status--true');
+        element.classList.remove('status--false');
+    }
+    else {
+        element.textContent = 'False';
+        element.classList.add('status--false');
+        element.classList.remove('status--true');
+    }
+}
+// Style the localhost differently.
+if (window.location.hostname === 'localhost') {
+    document.body.classList.add('localhost');
+}
 //# sourceMappingURL=example.js.map
