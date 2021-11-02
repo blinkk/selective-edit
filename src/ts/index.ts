7906f6c13a03b240fce20b8de07cce6e4a3e8f5a
Randy Merrill <randy@blinkk.com>
2021-11-02 08:45:28 -0600

    chore: Update deps.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -98,10 +98,18 @@ export {UuidMixin} from './mixins/uuid';
 // Cannot use lit-html libraries across different package installs.
 // Instead need to export any of the lit-html pieces here.
 // ex: Using the templates defined in selective in another library.
-export {Part, TemplateResult, directive, html, render} from 'lit-html';
+export {Part, TemplateResult, html, render} from 'lit-html';
+export {directive} from 'lit-html/directive';
+export {asyncAppend} from 'lit-html/directives/async-append';
+export {asyncReplace} from 'lit-html/directives/async-replace';
 export {classMap} from 'lit-html/directives/class-map';
+export {guard} from 'lit-html/directives/guard';
 export {ifDefined} from 'lit-html/directives/if-defined';
+export {live} from 'lit-html/directives/live';
+export {ref} from 'lit-html/directives/ref';
 export {repeat} from 'lit-html/directives/repeat';
 export {styleMap} from 'lit-html/directives/style-map';
+export {templateContent} from 'lit-html/directives/template-content';
 export {unsafeHTML} from 'lit-html/directives/unsafe-html';
+export {unsafeSVG} from 'lit-html/directives/unsafe-svg';
 export {until} from 'lit-html/directives/until';
